f1c7328f02de2a966153f197dbf4340e4eb8d562
IceInk <156809593@qq.com>
2020-08-17 10:42:39 +0800

    优化文件结果，添加Refletion帮助

---
+++ IceInk/Tools/00.CsharpExtensions/IEnumerable/ListExtension.cs
@@ -0,0 +1,74 @@
+﻿/**************************************************************************
+*   
+*   =================================
+*   CLR 版本    ：
+*   命名空间    ：IceInk.Extension
+*   文件名称    ：DictionaryExtension.cs
+*   =================================
+*   创 建 者    ：IceInk
+*   创建日期    ：2019/9/10 11:46:04 
+*   功能描述    ：
+*           List  的扩展使用
+*   使用说明    ：
+*   =================================
+*  
+***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace IceInk.Extension
+{
+    public static class EkListExtension
+    {
+        /// <summary>
+        /// 倒序遍历
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="selfList"></param>
+        /// <param name="action"></param>
+        /// <returns>返回自己</returns>
+        public static List<T> EkForEachReverse<T>(this List<T> selfList, Action<T> action)
+        {
+            if (action == null) throw new ArgumentException();
+            int count = selfList.Count;
+            for (var i = count - 1; i >= 0; --i)
+                action(selfList[i]);
+            return selfList;
+        }
+        /// <summary>
+        /// 倒序遍历(可获取索引)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="selfList"></param>
+        /// <param name="action"></param>
+        /// <returns>返回自己</returns>
+        public static List<T> EkForEachReverse<T>(this List<T> selfList, Action<T, int> action)
+        {
+            if (action == null) throw new ArgumentException();
+            int count = selfList.Count;
+            for (var i = count - 1; i >= 0; --i)
+                action(selfList[i], i);
+
+            return selfList;
+        }
+
+        /// <summary>
+        /// 遍历列表(可获得索引）
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="selfList"></param>
+        /// <param name="action"></param>
+        public static void EkForEach<T>(this List<T> selfList, Action<int, T> action)
+        {
+            if (selfList == null)
+                return;
+            int count = selfList.Count;
+            for (var i = 0; i < count; i++)
+            {
+                action(i, selfList[i]);
+            }
+        }
+
+    }
+}
